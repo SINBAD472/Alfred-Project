7fa1f7864ab06a30a3e651961f58f80ae5c5027e
SINBAD472 <calebnshang@gmail.com>
2026-04-19 22:13:57 +0000

    première intéraction avec l'assitant

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('bonjour maître Caleb')
+nom_du_butler = 'Alfred'
+version = 1.0
+
+print(f'système{nom_du_butler} , version{version}')
+print(f"initialisation du système en cours ...Prêt")
+
+nom_du_user = input('quel est votre nom ?: ')
+print(f'''Bonjour monsieur {nom_du_user}, comment
+ allez-vous aujourd'hui?''')
+print('Système en attente de commandes')
+
+
